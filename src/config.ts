3a924eaec3fde0d4ac480323be681fc506228b50
Javohir Hoshimov <javohirobbo@gmail.com>
2025-11-22 13:09:53 +0500

    Config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const API_BASE =
-  import.meta.env.VITE_API_BASE?.replace(/\/$/, '') || 'http://127.0.0.1:8000';
+  import.meta.env.VITE_API_BASE?.replace(/\/$/, '') || 'https://chatapi.sinoai.io';
 
 export type DateRange = {
   from: string;
